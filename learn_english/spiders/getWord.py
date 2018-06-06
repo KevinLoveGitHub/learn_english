afd2dc195f63edecc8f4601fdb1ef6539630bba1
Kevin <616651408@qq.com>
2018-06-06 18:37:25 +0800

    Update get desc

--- learn_english/spiders/getWord.py
+++ learn_english/spiders/getWord.py
@@ -6,7 +6,7 @@ from learn_english.word import Word, Comment
 class GetwordSpider(scrapy.Spider):
     name = 'getWord'
     allowed_domains = ['qiaoyingyu123.com']
-    start_urls = ['http://qiaoyingyu123.com/?p=380']
+    start_urls = ['http://qiaoyingyu123.com/?p=25']
 
     def parse(self, response):
         word = Word()
@@ -14,7 +14,7 @@ class GetwordSpider(scrapy.Spider):
         next_line = response.xpath('//span[@class="nav-next"]/a/@href').extract_first()
         word['nextLine'] = next_line
         word['comments'] = []
-        myList = response.xpath('//div[@class="dictionary-comment"]//p')
+        all_desc_text = response.xpath('//div[@class="dictionary-comment"]//text()')
         all_text = response.xpath('//div[@class="entry-content"]//text()').extract()
 
         for text in all_text:
@@ -22,13 +22,17 @@ class GetwordSpider(scrapy.Spider):
                 if len(text) > 3:
                     word['learn'] = text.strip().split('：', maxsplit=1)[1]
 
-        for p in myList:
-            if len(p.xpath('.//b').extract()) > 0:
+        for desc_selector in all_desc_text:
+            desc_text = desc_selector.extract()
+            if len(desc_text.strip()) > 0 and desc_text.endswith('.'):
+                # print('desc_text', desc_selector.extract())
                 comment = Comment()
-                comment['type'] = p.xpath('b/text()').extract_first()
-                comment['desc'] = p.xpath('text()').extract_first()
+                comment['type'] = desc_text
+                desc_index = all_desc_text.index(desc_selector) + 1
+                comment['desc'] = all_desc_text[desc_index].extract()
                 word['comments'].append(comment)
         # 如果使用管道， parse 必须返回要处理的数据
+        print(word)
         yield word
         if len(next_line):
             yield scrapy.Request(response.urljoin(next_line), self.parse)
