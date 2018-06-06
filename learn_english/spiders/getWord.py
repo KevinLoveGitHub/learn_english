a95abda656361e8e5c5a9fbf0263e5d212537f05
Kevin <616651408@qq.com>
2018-06-06 20:53:10 +0800

    Fix bug

--- learn_english/spiders/getWord.py
+++ learn_english/spiders/getWord.py
@@ -6,7 +6,7 @@ from learn_english.word import Word, Comment
 class GetwordSpider(scrapy.Spider):
     name = 'getWord'
     allowed_domains = ['qiaoyingyu123.com']
-    start_urls = ['http://qiaoyingyu123.com/?p=25']
+    start_urls = ['http://qiaoyingyu123.com/?p=144']
 
     def parse(self, response):
         word = Word()
@@ -14,10 +14,12 @@ class GetwordSpider(scrapy.Spider):
         next_line = response.xpath('//span[@class="nav-next"]/a/@href').extract_first()
         word['nextLine'] = next_line
         word['comments'] = []
-        last_sample_target = response.xpath('//p[@class="sample-target"][last()]/text()').extract_first().strip()
+        last_sample_target = response.xpath('//p[@class="sample-target"]/text()').extract_first()
+        last_p = response.xpath('//div[@class="dictionary-comment"]/p[last()]/text()').extract()[-1]
         all_text = response.xpath('//div[@class="entry-content"]//text()').extract()
         all_desc = response.xpath('//div[@class="dictionary-comment"]//p[not(@class)]//text()').extract()
-
+        print(last_sample_target)
+        print(last_p)
         for text in all_text:
             # print('text', text)
             if '巧记：' in text or '注：' in text:
@@ -29,15 +31,24 @@ class GetwordSpider(scrapy.Spider):
                 comment['type'] = text
                 desc_index = all_text.index(text) + 1
                 comment['desc'] = all_text[desc_index]
-                print(comment['type'])
-                print(comment['desc'])
                 word['comments'].append(comment)
 
         # 判断对象中是否存在元素
         if word.get('learn') is None:
-            word['learn'] = last_sample_target
+            if last_sample_target is not None:
+                word['learn'] = last_sample_target.strip()
+            elif last_p is not None:
+                word['learn'] = last_p.strip()
+
+        news_comments = []
+        for comment in word['comments']:
+            if comment not in news_comments:
+                news_comments.append(comment)
+
+        word['comments'] = news_comments
+        print(word)
 
         # 如果使用管道， parse 必须返回要处理的数据
-        yield word
-        if len(next_line):
-            yield scrapy.Request(response.urljoin(next_line), self.parse)
+        # yield word
+        # if len(next_line):
+        #     yield scrapy.Request(response.urljoin(next_line), self.parse)
